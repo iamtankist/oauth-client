eef653f60aa8d9057a29b8dbb3f17039ab8810c6
iamtankist <tankist@gmail.com>
2014-03-04 16:53:08 +0100

    Update AuthController.php

--- src/StarkIndustries/ClientBundle/Controller/AuthController.php
+++ src/StarkIndustries/ClientBundle/Controller/AuthController.php
@@ -3,13 +3,12 @@
 namespace StarkIndustries\ClientBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 // these import the "@Route" and "@Template" annotations
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 use OAuth2;
 
@@ -18,7 +17,6 @@ class AuthController extends Controller
 {
     /**
      * @Route("/authorize", name="auth")
-     * @Template()
      */
     public function authAction(Request $request)
     {
@@ -29,8 +27,7 @@ class AuthController extends Controller
         }
 
         $authorizeClient->getAccessToken($request->query->get('code'));
-        var_dump($authorizeClient->fetch('http://oauth-server.local/api/articles'));exit;
 
-        return new JsonResponse();
+        return new Response($authorizeClient->fetch('http://oauth-server.local/api/articles'));
     }
 }
